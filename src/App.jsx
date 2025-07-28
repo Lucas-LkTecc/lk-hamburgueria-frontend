e3958f3893b08b4c7f68d1f4870b1de8815b47a2
Lucas-LkTecc <ruan.alencar97@gmail.com>
2025-07-28 15:13:17 -0300

    feat: initial setup with Vite and React

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      {/* Aqui é onde o código do cabeçalo e outras seções irão */}
+      <h1>LK Hamburgueria Landing Page</h1>
+    </>
+  );
+}
+
+export default App;
